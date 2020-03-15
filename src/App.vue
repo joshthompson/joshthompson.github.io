1763235076e4cb62b8a496bbaa7f8ddc18859a9f
Josh Thompson <josh3276@gmail.com>
2020-03-15 22:17:04 +0100

    Installed Vue

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<script lang="ts">
+	import { Component, Vue } from 'vue-property-decorator'
+
+	@Component
+	export default class App extends Vue {}
+</script>
+
+<template>
+	<div id="app">
+		<div class="main">
+			<router-view />
+		</div>
+		<footer>
+			<menu>
+				Josh Thompson
+			</menu>
+		</footer>
+	</div>
+</template>
+
+<style lang="scss">
+	body {
+		font-family: 'Inter', sans-serif;
+		margin: 0;
+	}
+
+	#app {
+		display: flex;
+		flex-direction: column;
+		height: 100vh;
+	}
+
+	.main {
+		flex-grow: 1;
+		background: #262933;
+		color: #FFFFFF;
+	}
+
+	footer {
+		background: #363944;
+		color: #FFFFFF;
+
+		menu {
+			margin: 0;
+			padding: 1rem;
+		}
+	}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import { Component, Vue } from 'vue-property-decorator'
+
+	@Component
+	export default class Home extends Vue {}
+</script>
+
+<template>
+	<div class="home">
+		<h1>Hire me</h1>
+		<p>josh3276 at gmail dot com</p>
+	</div>
+</template>
+
+<style lang="scss" scoped>
+	h1 {
+		margin-top: 0;
+	}
+	.home {
+		padding: 1rem;
+	}
+</style>
