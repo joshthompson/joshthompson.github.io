0c2091f33b01dcf2f26c1c3f59855313d26b561a
Josh Thompson <josh3276@gmail.com>
2020-03-23 16:29:38 +0100

    Movement changed

--- src/components/Me3D.vue
+++ src/components/Me3D.vue
@@ -7,7 +7,7 @@
 		y: number
 	}
 
-	function minmax(value, max = 8) {
+	function minmax(value, max = 10) {
 		return Math.min(max, Math.max(-max, value))
 	}
 
@@ -15,7 +15,7 @@
 	export default class Me3D extends Vue {
 
 		private offset: Vector = { x: 0, y: 0 }
-		private movement: number = 5
+		private movement: Vector = { x: 10, y: 10 }
 		public size: number = 300
 		private phone: boolean = false
 		private orientation: DeviceOrientationEvent = null
@@ -27,23 +27,15 @@
 			window.addEventListener('deviceorientation', this.setOffsetPhone)
 		}
 
-		private get position() {
-			const container = this.$refs.container as HTMLElement
-			return {
-				x: container.getBoundingClientRect().left + this.size / 2,
-				y: container.getBoundingClientRect().top + this.size / 2
-			}
-		}
-
 		private setOffset(event: MouseEvent) {
 			if (!this.phone) {
-				const diff: Vector = {
-					x: event.pageX - this.position.x,
-					y: event.pageY - this.position.y
-				}
+				const multiplier: Vector = {
+					x: minmax((event.pageX / window.innerWidth) * 2 - 1, 1),
+					y: minmax((event.pageY / window.innerHeight) * 2 - 1, 1)
+				} // A number between -1 and 1 - min maxed as cursor can go beyond and still register
 				this.offset = {
-					x: minmax(diff.x / (this.size / 2) * this.movement),
-					y: minmax(diff.y / (this.size / 2) * this.movement)
+					x: multiplier.x * this.movement.x,
+					y: multiplier.y * this.movement.y
 				}
 			}
 		}
@@ -51,8 +43,8 @@
 		private setOffsetPhone(event: DeviceOrientationEvent) {
 			this.phone = true
 			if (this.orientation) {
-				this.offset.x = minmax(this.offset.x + this.orientation.alpha - event.alpha)
-				this.offset.y = minmax(this.offset.y + this.orientation.beta - event.beta)
+				this.offset.x = minmax(this.offset.x + this.orientation.alpha - event.alpha, this.movement.x)
+				this.offset.y = minmax(this.offset.y + this.orientation.beta - event.beta, this.movement.y)
 			}
 			this.orientation = event
 		}
@@ -80,7 +72,7 @@
 </script>
 
 <template>
-	<div ref="container" class="image-3d" :style="containerStyle">
+	<div class="image-3d" :style="containerStyle">
 		<!-- <pre>{{ debug }}</pre> -->
 		<img class="foreground" :style="foregroundStyle" src="@/assets/me-fg.png" />
 		<img class="background" :style="backgroundStyle" src="@/assets/me-bg.png" />
